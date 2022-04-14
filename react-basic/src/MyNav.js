ff4008c6f16dc026d9379dc37cfe1b7632ad2a65
Eunseo Choi <yses9296@gmail.com>
2022-04-14 17:20:36 +0900

    react 15

--- react-basic/src/MyNav.js
+++ react-basic/src/MyNav.js
@@ -4,7 +4,13 @@ export default class MyNav extends Component {
     render() {
         var lists = [];
         var data = this.props.data;
-        
+
+        var i = 0;
+        while (i < data.length){
+            lists.push(<li key={data[i].id}><a href="#">{data[i].title}</a></li>);
+            i++;
+        }
+
         return (
         <nav>
             <ul>
