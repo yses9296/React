709569805ec5d6c68a3152cb8d95e11d606fa8f5
Eunseo Choi <yses9296@gmail.com>
2022-06-12 19:41:13 +0900

    react 40

--- react_ajax/src/App.js
+++ react_ajax/src/App.js
@@ -2,41 +2,46 @@ import './App.css';
 import React, { Component } from 'react';
 
 class Nav extends Component {
+  // state = {
+  //   list: []
+  // }
   constructor(props){
     super(props);
     this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
+      list: []
     }
   }
   componentDidMount(){
     fetch('./data/list.json')
-    .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result.items
-          });
-        },//json data를 받아왔을 시, 성공했을 시.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }//error가 있을 시, 데이터를 읽는데 실패할 시.
-      )
+    .then( result => result.json() )
+    .then((data) => {
+      this.setState({list: data})
+    });
+
   }
   
   render() {
+
+    let listTages = [];
+    var list = this.state.list;
+    /* 
+    this.state.list
+    for 반복문으로 
+    */
+    for (let i = 0; i < list.length; i++ ){
+      listTages.push(
+        <li><a href={list[i].id}>{list[i].title}</a></li>
+      )
+    }
+    
     return (
 
          <nav>
           <ul>
-            <li><a href="1">HTML</a></li>
+            {/* <li><a href="1">HTML</a></li>
             <li><a href="2">CSS</a></li>
-            <li><a href="3">JS</a></li>
+            <li><a href="3">JS</a></li> */}
+            {listTages}
           </ul>
         </nav>
 
